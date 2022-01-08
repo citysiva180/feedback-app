924b174684de0567aad75305da5da74ca6a5f21a
Siva Rajan <citysiva180@gmail.com>
2022-01-08 21:07:10 +0530

    Completed the rating component

--- src/components/FeedbackItems.jsx
+++ src/components/FeedbackItems.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Card from "./Shared/Card"
 import PropTypes from 'prop-types'
 import {FaTimes} from "react-icons/fa"
-function FeedbackItems({item}) {
+function FeedbackItems({item, handleDelete}) {
     
+   
     return (
         <Card reverse={true}>
             <div className="num-display">{item.rating}</div>
-            <button className="close"><FaTimes color="purple"></FaTimes></button>
+            <button className="close" onClick={()=>handleDelete(item.id)}><FaTimes color="purple"></FaTimes></button>
             <div className="text display">{item.text}</div>
          
         </Card>
