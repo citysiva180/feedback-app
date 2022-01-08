afede45da0ec80646dc36a192df961455b579a36
Siva Rajan <citysiva180@gmail.com>
2022-01-08 10:24:55 +0530

    completed props and styling components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="container">
-      <h1>App Starting</h1>
-      <p>Learning React Completly from Scratch 2022</p>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <h1>App Starting</h1>
+        <p>Learning React Completly from Scratch 2022</p>
+      </div>
+    </>
   );
 }
 
